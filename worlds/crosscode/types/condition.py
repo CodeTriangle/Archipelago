dade03ed647b7b48302c70431d07c1026527f071
Richard Snider <lighthousemaniac@gmail.com>
2025-07-30 02:18:10 -0600

    create lambda condition prototype

--- worlds/crosscode/types/condition.py
+++ worlds/crosscode/types/condition.py
@@ -1,6 +1,6 @@
 import typing
 import abc
-from dataclasses import field, dataclass
+from dataclasses import dataclass
 
 from BaseClasses import CollectionState
 from ..options import ShopReceiveMode
@@ -22,7 +22,7 @@ class LogicDict(typing.TypedDict):
 
 class Condition(abc.ABC):
     @abc.abstractmethod
-    def satisfied(self, state: CollectionState, player: int, location: int | None, args: LogicDict) -> bool:
+    def satisfied(self, player: int, location: int | None, args: LogicDict) -> typing.Callable[[CollectionState], bool]:
         pass
 
 @dataclass
@@ -30,7 +30,7 @@ class ItemCondition(Condition):
     item_name: str
     amount: int = 1
 
-    def satisfied(self, state: CollectionState, player: int, location: int | None, args: LogicDict) -> bool:
+    def satisfied(self, player: int, location: int | None, args: LogicDict) -> typing.Callable[[CollectionState], bool]:
         target = self.amount
         if self.item_name in args["keyrings"]:
             target = 1
@@ -38,32 +38,36 @@ class ItemCondition(Condition):
         replacements = args["item_progressive_replacements"]
 
         if self.item_name in replacements:
-            for prog_item_name, quantity in replacements[self.item_name]:
-                if state.has(prog_item_name, player, quantity):
-                    return True
+            def callback(state: CollectionState) -> bool:
+                for prog_item_name, quantity in replacements[self.item_name]:
+                    if state.has(prog_item_name, player, quantity):
+                        return True
+                return  state.has(self.item_name, player, target)
 
-        return state.has(self.item_name, player, target)
+            return callback
+
+        return lambda state: state.has(self.item_name, player, target)
 
 @dataclass
 class QuestCondition(Condition):
     quest_name: str
 
-    def satisfied(self, state: CollectionState, player: int, location: int | None, args: LogicDict) -> bool:
-        return state.has(f"{self.quest_name} (Event)", player)
+    def satisfied(self, player: int, location: int | None, args: LogicDict) -> typing.Callable[[CollectionState], bool]:
+        return lambda state: state.has(f"{self.quest_name} (Event)", player)
 
 @dataclass
 class LocationCondition(Condition):
     location_name: str
 
-    def satisfied(self, state: CollectionState, player: int, location: int | None, args: LogicDict) -> bool:
-        return state.has(f"{self.location_name} (Event)", player)
+    def satisfied(self, player: int, location: int | None, args: LogicDict) -> typing.Callable[[CollectionState], bool]:
+        return lambda state: state.has(f"{self.location_name} (Event)", player)
 
 @dataclass
 class RegionCondition(Condition):
     target_mode: typing.Optional[str]
     region_name: str
 
-    def satisfied(self, state: CollectionState, player: int, location: int | None, args: LogicDict) -> bool:
+    def satisfied(self, player: int, location: int | None, args: LogicDict) -> typing.Callable[[CollectionState], bool]:
         # target_mode == None means that it matches in all modes.
         # so if target_mode == None, check if we can reach that region.
         # else, if the target mode matches, also check if we can reach that region.
@@ -71,14 +75,14 @@ class RegionCondition(Condition):
 
         mode: str = args["mode"]
         if self.target_mode is None or mode == self.target_mode:
-            return state.can_reach_region(self.region_name, player)
+            return lambda state: state.can_reach_region(self.region_name, player)
 
-        return True
+        return lambda _: True
 
 @dataclass
 class AnyElementCondition(Condition):
-    def satisfied(self, state: CollectionState, player: int, location: int | None, args: LogicDict) -> bool:
-        return any([
+    def satisfied(self, player: int, location: int | None, args: LogicDict) -> typing.Callable[[CollectionState], bool]:
+        return lambda state: any([
             state.has("Heat", player),
             state.has("Cold", player),
             state.has("Shock", player),
@@ -89,8 +93,9 @@ class AnyElementCondition(Condition):
 class OrCondition(Condition):
     subconditions: list[Condition]
 
-    def satisfied(self, state: CollectionState, player: int, location: int | None, args: LogicDict) -> bool:
-        return any(map(lambda x: x.satisfied(state, player, location, args), self.subconditions))
+    def satisfied(self, player: int, location: int | None, args: LogicDict) -> typing.Callable[[CollectionState], bool]:
+        callbacks = [x.satisfied(player, location, args) for x in self.subconditions]
+        return lambda state: any(map(lambda x: x(state), callbacks))
 
 @dataclass
 class AndCondition(Condition):
@@ -103,18 +108,24 @@ class AndCondition(Condition):
 class VariableCondition(Condition):
     name: str
 
-    def satisfied(self, state: CollectionState, player: int, location: int | None, args: LogicDict) -> bool:
+    def satisfied(self, player: int, location: int | None, args: LogicDict) -> typing.Callable[[CollectionState], bool]:
         variables = args["variables"]
         variable_definitions = args["variable_definitions"]
 
         if self.name not in variables:
-            return True
+            return lambda _: True
 
-        for value in variables[self.name]:
-            if not all(map(lambda c: c.satisfied(state, player, location, args), variable_definitions[self.name][value])):
-                return False
+        callbacks = sum(
+            [
+                [
+                    x.satisfied(player, location, args)
+                    for x in variable_definitions[self.name][value]
+                ] for value in variables[self.name]
+            ],
+            start=[]
+        )
 
-        return True
+        return lambda state: all(map(lambda x: x(state), callbacks))
 
 @dataclass
 class VariableEntryCondition(Condition):
@@ -137,7 +148,7 @@ class ChestKeyCondition(Condition):
         "Gold": "Radiant Key",
     }
 
-    def satisfied(self, state: CollectionState, player: int, location: int | None, args: LogicDict) -> bool:
+    def satisfied(self, player: int, location: int | None, args: LogicDict) -> typing.Callable[[CollectionState], bool]:
         chest_levels = args["chest_clearance_levels"]
 
         if location is None:
@@ -146,25 +157,25 @@ class ChestKeyCondition(Condition):
         level: str = chest_levels.get(location, self.default_level)
 
         if level == "Default":
-            return True
+            return lambda _: True
 
-        return state.has(ChestKeyCondition.clearance_items[level], player)
+        return lambda state: state.has(ChestKeyCondition.clearance_items[level], player)
 
 @dataclass
 class ShopSlotCondition(Condition):
     shop_name: str
     item_id: int
 
-    def satisfied(self, state: CollectionState, player: int, location: int | None, args: LogicDict) -> bool:
+    def satisfied(self, player: int, location: int | None, args: LogicDict) -> typing.Callable[[CollectionState], bool]:
         if args["shop_receive_mode"] is None:
-            return True
+            return lambda _: True
         if args["shop_receive_mode"] == ShopReceiveMode.option_per_item_type:
-            return state.has(args["shop_unlock_by_id"][self.item_id].name, player)
+            return lambda state: state.has(args["shop_unlock_by_id"][self.item_id].name, player)
         if args["shop_receive_mode"] == ShopReceiveMode.option_per_shop:
-            return state.has(args["shop_unlock_by_shop"][self.shop_name].name, player)
+            return lambda state: state.has(args["shop_unlock_by_shop"][self.shop_name].name, player)
         if args["shop_receive_mode"] == ShopReceiveMode.option_per_slot:
-            return state.has(args["shop_unlock_by_shop_and_id"][self.shop_name, self.item_id].name, player)
-        return True
+            return lambda state: state.has(args["shop_unlock_by_shop_and_id"][self.shop_name, self.item_id].name, player)
+        return lambda _: True
 
 @dataclass
 class BotanicsCompletionCondition(Condition):
@@ -180,8 +191,8 @@ class BotanicsCompletionCondition(Condition):
         return collected >= self.amount
 
 class NeverCondition(Condition):
-    def satisfied(self, state: CollectionState, player: int, location: int | None, args: LogicDict) -> bool:
-        return False
+    def satisfied(self, player: int, location: int | None, args: LogicDict) -> typing.Callable[[CollectionState], bool]:
+        return lambda _: False
 
 __all__ = [
     "Condition",
