a4cb460cf3edcda74273e9c9f476440f74b65169
Richard Snider <lighthousemaniac@gmail.com>
2026-08-29 00:11:22 -0600

    codegen for CodeTriangle/CCMultiworldRandomizer#93

--- worlds/crosscode/regions.py
+++ worlds/crosscode/regions.py
@@ -9,56 +9,12 @@ from .types.regions import Goal, RegionConnection, RegionsData
 from .types.condition import *
 
 modes = [
-    'linear',
     'open',
 ]
 
 default_mode = "open"
 
 region_packs: typing.Dict[str, RegionsData] = {
-    "linear": RegionsData(
-        starting_region = "2",
-        excluded_regions = ['1'],
-        region_connections = [
-            RegionConnection(region_from='2', region_to='3', cond=[ItemCondition(item_name='Green Leaf Shade', amount=1)]),
-            RegionConnection(region_from='3', region_to='4', cond=[ItemCondition(item_name='Mine Pass', amount=1), ItemCondition(item_name='Guild Pass', amount=1)]),
-            RegionConnection(region_from='3', region_to='3.1', cond=[ItemCondition(item_name='Mine Pass', amount=1)]),
-            RegionConnection(region_from='4', region_to='5', cond=[ItemCondition(item_name='Mine Key', amount=1)]),
-            RegionConnection(region_from='5', region_to='6', cond=[ItemCondition(item_name='Mine Key', amount=2)]),
-            RegionConnection(region_from='6', region_to='7', cond=[ItemCondition(item_name='Mine Key', amount=3)]),
-            RegionConnection(region_from='7', region_to='8', cond=[ItemCondition(item_name='Mine Key', amount=4)]),
-            RegionConnection(region_from='7', region_to='9', cond=[ItemCondition(item_name="Thief's Key", amount=1), ItemCondition(item_name='Heat', amount=1)]),
-            RegionConnection(region_from='9', region_to='10', cond=[ItemCondition(item_name='Mine Key', amount=5)]),
-            RegionConnection(region_from='10', region_to='11', cond=[ItemCondition(item_name='Mine Master Key', amount=1), ItemCondition(item_name='Blue Ice Shade', amount=1)]),
-            RegionConnection(region_from='11', region_to='12', cond=[ItemCondition(item_name='Maroon Cave Pass', amount=1)]),
-            RegionConnection(region_from='12', region_to='13', cond=[ItemCondition(item_name='Yellow Sand Shade', amount=1), ItemCondition(item_name='Heat', amount=1)]),
-            RegionConnection(region_from='13', region_to='14', cond=[ItemCondition(item_name="Faj'ro Key", amount=1)]),
-            RegionConnection(region_from='14', region_to='15', cond=[ItemCondition(item_name="Faj'ro Key", amount=3)]),
-            RegionConnection(region_from='15', region_to='16', cond=[ItemCondition(item_name="Faj'ro Key", amount=4)]),
-            RegionConnection(region_from='16', region_to='17', cond=[ItemCondition(item_name='Cold', amount=1)]),
-            RegionConnection(region_from='17', region_to='17.5', cond=[ItemCondition(item_name='White Key', amount=1)]),
-            RegionConnection(region_from='17.5', region_to='18', cond=[ItemCondition(item_name="Faj'ro Key", amount=9)]),
-            RegionConnection(region_from='17.5', region_to='19', cond=[ItemCondition(item_name="Faj'ro Master Key", amount=1)]),
-            RegionConnection(region_from='19', region_to='20', cond=[ItemCondition(item_name='Red Flame Shade', amount=1)]),
-            RegionConnection(region_from='20', region_to='21', cond=[ItemCondition(item_name='Green Seed Shade', amount=1)]),
-            RegionConnection(region_from='21', region_to='23', cond=[]),
-            RegionConnection(region_from='21', region_to='24', cond=[ItemCondition(item_name='Pond Slums Pass', amount=1)]),
-            RegionConnection(region_from='23', region_to='25', cond=[ItemCondition(item_name="Zir'vitar Key", amount=2)]),
-            RegionConnection(region_from='23', region_to='26', cond=[ItemCondition(item_name="So'najiz Key", amount=1)]),
-            RegionConnection(region_from='26', region_to='26.5', cond=[ItemCondition(item_name="So'najiz Key", amount=3)]),
-            RegionConnection(region_from='26', region_to='27', cond=[ItemCondition(item_name='Radiant Key', amount=1), ItemCondition(item_name="So'najiz Key", amount=4), ItemCondition(item_name='Shock', amount=1)]),
-            RegionConnection(region_from='23', region_to='28', cond=[ItemCondition(item_name='Azure Drop Shade', amount=1), ItemCondition(item_name='Purple Bolt Shade', amount=1), ItemCondition(item_name='Wave', amount=1), ItemCondition(item_name='Shock', amount=1)]),
-            RegionConnection(region_from='28', region_to='29', cond=[ItemCondition(item_name="Krys'kajo Key", amount=2)]),
-            RegionConnection(region_from='28', region_to='30', cond=[ItemCondition(item_name='Kajo Master Key', amount=1)]),
-            RegionConnection(region_from='30', region_to='31', cond=[ItemCondition(item_name='Star Shade', amount=1)]),
-            RegionConnection(region_from='31', region_to='32', cond=[ItemCondition(item_name='Old Dojo Key', amount=1)]),
-            RegionConnection(region_from='32', region_to='33', cond=[ItemCondition(item_name='Meteor Shade', amount=1)]),
-            RegionConnection(region_from='31', region_to='22', cond=None),
-        ],
-        goals = {
-            'creator': Goal(region='32', condition=None),
-        }
-    ),
     "open": RegionsData(
         starting_region = "open2",
         excluded_regions = ['open1'],
